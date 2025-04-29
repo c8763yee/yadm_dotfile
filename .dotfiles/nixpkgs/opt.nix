f2414381d27e56f0fe66e0589bf5d26b9dce15e9
c8763yee <rrrrr4413@gmail.com>
2025-04-29 15:53:31 +0800

    remove dotfiles

--- .dotfiles/nixpkgs/opt.nix
+++
@@ -1,9 +0,0 @@
-let
-  homeDirectory = builtins.getEnv "HOME";  # 使用环境变量 HOME
-  optConfig = if (builtins.pathExists "${homeDirectory}/opt-local.nix")
-    then import "${homeDirectory}/opt-local.nix"
-    else {};
-in
-{
-  isGui = true;
-} // optConfig
--- .dotfiles/scripts/nix/env/gnuefi.nix
+++
@@ -1,9 +0,0 @@
-{ pkgs ? import <nixpkgs> { } }:
-
-  pkgs.stdenv.mkDerivation {
-    name = "gnuefi";
-    buildInputs = with pkgs; [
-      gnu-efi
-      pkg-config
-    ];
-  }
