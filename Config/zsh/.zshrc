32962d4d43d223f9a69cd4829f4bab3db4e389e3
c8763yee <rrrrr4413@gmail.com>
2025-11-11 12:58:59 +0800

    feat: Use another nvim dotfile

---
+++ Config/zsh/.zshrc
@@ -0,0 +1,33 @@
+
+if [[ $- =~ i ]] && [[ -z "$TMUX" ]] && [[ -n "$SSH_TTY" ]]; then
+  tmux attach-session -t ssh_tmux || tmux new-session -s ssh_tmux
+fi
+
+# PATH settings
+export PATH=/home/c8763yee/.local/bin${PATH:+:${PATH}}
+if [[ -n $(command -v snap 2> /dev/null) ]]; then
+	export PATH=/snap/bin:$PATH
+fi
+
+if [[ -n $(command -v nvidia-smi 2> /dev/null) ]]; then
+	export PATH=/opt/cuda/bin:$PATH
+	export LD_LIBRARY_PATH=/opt/cuda/lib64:${LD_LIBRARY_PATH:+:$LD_LIBRARY_PATH}
+fi
+
+# If you come from bash you might have to change your $PATH.
+export PATH=$HOME/bin:/usr/local/bin:$PATH
+
+# Format for `time` command in terminal
+TIMEFMT=$(cat <<EOF
+-----------------------
+Time Info: %3J
+real	%E
+user	%U
+sys	%S
+%P cpu %(%Xtext+%Ddata %Mmax)k
+%I inputs+%O outputs (%F major+%R minor) pagefaults %
+Wswaps %*E total
+-----------------------
+EOF
+)
+
---
+++ Scripts/install.sh
@@ -0,0 +1,123 @@
+#!/bin/bash
+: <<COMMENT
+This script is used to install the required packages for yadm bootstrap
+
+Available distro: Arch, Debian, Fedora, Ubuntu
+Package:
+- yadm
+- git
+- curl
+- zsh
+- neovim
+- wget
+- tmux
+- vim plugins required package
+
+Plugin
+- oh-my-zsh
+- zsh-autosuggestions
+- zsh-syntax-highlighting
+- zsh-completions
+- powerlevel10k
+- linux_config
+- oh-my-tmux
+COMMENT
+
+PACKAGES=(
+	"zsh"
+	"git"
+	"curl"
+	"neovim"
+	"wget"
+	"tmux"
+	"fzf"
+	"find"
+	"ripgrep"
+	"clang"
+	"gcc"
+	"nodejs"
+	"npm"
+	"luarocks"
+	"yarn"
+	"s-tui"
+	"pre-commit"
+)
+
+function install_yay(){
+	sudo pacman -S --noconfirm base-devel
+	git clone https://aur.archlinux.org/yay.git
+	cd yay
+	makepkg -si --noconfirm
+}
+
+function install_required_packages() {
+	distro=$(cat /etc/os-release | grep -w "ID" | cut -d "=" -f 2 | tr -d '"')
+	case $distro in
+	"arch")
+		sudo pacman -Syu --noconfirm
+		sudo pacman -S --noconfirm ${PACKAGES[@]}
+
+		# Extra packages
+		install_yay
+		sudo pacman -S --noconfirm lua51 rustup cargo rust-analyzer tree-sitter{,-cli} \
+			hyprland hyprpaper swaylock waybar nwg-{look,displays,dock-hyprland}\
+			gnome-keyring
+		rustup install stable
+		;;
+    "msys2")
+		pacman -Syu --noconfirm
+		pacman -S --noconfirm ${PACKAGES[@]}
+        ;;
+	"debian" | "ubuntu")
+		sudo apt update
+		sudo apt install -y ${PACKAGES[@]}
+		sudo apt install -y fd-find
+		;;
+	"fedora")
+		sudo dnf update -y
+		sudo dnf install -y ${PACKAGES[@]}
+		;;
+	*)
+		echo "Unsupported distro"
+		exit 1
+		;;
+	esac
+}
+
+function install_oh_my_tmux(){
+	# install in $XDG_CONFIG_HOME/tmux
+	if [[ -f $XDG_CONFIG_HOME/tmux/.tmux.conf ]]; then
+		echo "Oh My Tmux is already installed."
+		return
+	fi
+	echo "Installing Oh My Tmux..."
+	git clone https://github.com/gpakosz/.tmux.git
+	mkdir -p $XDG_CONFIG_HOME/tmux
+	ln -s .tmux/.tmux.conf $XDG_CONFIG_HOME/tmux/.tmux.conf
+}
+
+function setup_zsh() {
+	# Change default shell to zsh
+	if [[ $SHELL != *"zsh"* ]]; then
+		chsh -s $(which zsh)
+	fi
+}
+
+function move_config() {
+	ln -s Config/zsh $HOME/.config/zsh
+	ln -s Config/nvim $HOME/.config/nvim
+	ln -s Config/tmux/.tmux.conf $XDG_CONFIG_HOME/tmux/.tmux.conf
+	ln -s Config/gdb/.gdbinit $HOME/.gdbinit
+	ln -s Config/hypr $XDG_CONFIG_HOME/hypr
+	ln -s Config/waybar $XDG_CONFIG_HOME/waybar
+}
+
+function main() {
+	install_required_packages
+	install_oh_my_tmux
+	setup_zsh
+	move_config
+}
+
+main
+echo "Done"
